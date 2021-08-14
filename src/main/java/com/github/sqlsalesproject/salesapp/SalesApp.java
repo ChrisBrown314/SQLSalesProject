1b80d8b72e05f3d0623a2305cc528323dfb1a132
ChrisBrown314 <88871929+ChrisBrown314@users.noreply.github.com>
2021-08-13 23:25:27 -0500

    Fixed fxml and resource issues, refactored and cleaned up resources folder. Got JavaFX set up so I can start designing scenes for it when I get there.

--- src/main/java/com/github/sqlsalesproject/salesapp/SalesApp.java
+++ src/main/java/com/github/sqlsalesproject/salesapp/SalesApp.java
@@ -8,13 +8,21 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class SalesApp extends Application {
+    /** Loads fxml scene, sets window properties, and shows the window*/
     @Override
     public void start(Stage stage) throws IOException {
-        //TODO fix fxml issues
+
+        FXMLLoader sceneLoader = new FXMLLoader(getClass().getResource("/fxmlResources/sales-app.fxml"));
+        Scene scene = new Scene(sceneLoader.load(), 320, 240);
+
+        stage.setScene(scene);
+        stage.setTitle("Sales Examination App");
+        stage.show();
     }
 
+    /** Connects to Database and launches the application*/
     public static void main(String[] args) {
         Database.getDatabase();
-        //launch();
+        launch();
     }
 }
