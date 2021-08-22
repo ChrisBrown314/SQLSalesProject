05b3d613ea76d1c7f82a1193b0531396c5ad8686
ChrisBrown314 <88871929+ChrisBrown314@users.noreply.github.com>
2021-08-21 22:34:00 -0500

    Fleshed out database class with the new purchase history changes. Cleaned everything up and refactored the fetchPurchaseHistory method to be much simpler and hopefully a little easier to follow.

--- src/main/java/com/github/sqlsalesproject/databasemanagement/Database.java
+++ src/main/java/com/github/sqlsalesproject/databasemanagement/Database.java
@@ -11,12 +11,12 @@ import java.util.ArrayList;
 
 public class Database {
     //Program requires mysql server set up on port 3306 with a user having this username and
-    //password and a salesproject schema1
+    //password and a salesproject schema
     private static final String CONNECTION_INFO = "jdbc:mysql://localhost:3306/salesproject";
     private static final String DB_USERNAME = "salesproject";
     private static final String DB_PASSWORD = "sR3G>[d2ZWqF6p54";
     private static Connection Connection;
-    private static Statement Statement;
+
 
     //Singleton Implementation//
     private static Database databaseInstance = null;
@@ -24,7 +24,6 @@ public class Database {
     private Database() {
         try {
             Connection = DriverManager.getConnection(CONNECTION_INFO,DB_USERNAME, DB_PASSWORD);
-            Statement = Connection.createStatement();
             //Check if table exists for purchase data and if not, creates it
             if (!purchaseTableExists()) {
                 createPurchaseTable();
@@ -32,10 +31,10 @@ public class Database {
             if (!historyTableExists()) {
                 createHistoryTable();
             }
-        } catch (SQLException sql_exception) {
+        } catch (SQLException sqlException) {
             //TODO logging and better error handling
             System.err.println("ERR: Failed to connect to SQL database. Quitting!");
-            System.err.println(sql_exception.getMessage());
+            System.err.println(sqlException.getMessage());
             System.exit(-1);
         }
     }
@@ -46,128 +45,161 @@ public class Database {
         }
         return databaseInstance;
     }
-    public static void close(){
-        try {
-            Connection.close();
-            Statement.close();
-        } catch (SQLException sql_exception) {
-            System.err.println("ERR: Failed to close database resources!");
-            System.err.println(sql_exception.getMessage());
-        }
-    }
 
-    //TODO - figure out what to do with all these exceptions!
-    //TODO - Also figure out the statement stuff
-    //TODO - maybe also separate into separate classes
+
     //Table Handling//
     /** Returns true if the purchase table exists */
     private boolean purchaseTableExists() throws SQLException {
-        ResultSet resultSet = Statement.executeQuery("SELECT EXISTS( SELECT * FROM " +
+        Statement statement = Connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT EXISTS( SELECT * FROM " +
                 "information_schema.TABLES WHERE TABLE_NAME = 'purchase');");
         resultSet.next(); //Have to do this prior to reading the result set, or the program errors
         boolean exists = (resultSet.getInt(1) == 1);
         resultSet.close();
+        statement.close();
         return exists;
     }
+    /** Creates the purchase table in the database */
     private void createPurchaseTable() throws SQLException {
-        Statement.executeUpdate("CREATE TABLE purchase ( date TINYTEXT NOT NULL, month INT NOT NULL," +
+        Statement statement = Connection.createStatement();
+        statement.executeUpdate("CREATE TABLE purchase ( date TINYTEXT NOT NULL, month INT NOT NULL," +
                 " burger INT NOT NULL, sandwich INT NOT NULL, strip INT NOT NULL);");
+        statement.close();
 
     }
+    /** Checks if the history table exists */
     private boolean historyTableExists() throws SQLException {
-        ResultSet resultSet = Statement.executeQuery("SELECT EXISTS( SELECT * FROM " +
+        Statement statement = Connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT EXISTS( SELECT * FROM " +
                 "information_schema.TABLES WHERE TABLE_NAME = 'history');");
         resultSet.next(); //Have to do this prior to reading the result set, or the program errors
         boolean exists = (resultSet.getInt(1) == 1);
         resultSet.close();
+        statement.close();
         return exists;
     }
+    /** Creates the history table in the database*/
     private void createHistoryTable() throws SQLException {
-        Statement.executeUpdate("CREATE TABLE history ( month INT NOT NULL," +
+        Statement statement = Connection.createStatement();
+        statement.executeUpdate("CREATE TABLE history ( month INT NOT NULL," +
                 " supplyCost DOUBLE NOT NULL);");
+        statement.close();
 
     }
+    /** Deletes all data from the database*/
     public void clearTables() throws SQLException {
-        Statement.executeUpdate("DELETE FROM purchase");
-        Statement.executeUpdate("DELETE FROM history");
+        Statement statement = Connection.createStatement();
+        statement.executeUpdate("DELETE FROM purchase");
+        statement.executeUpdate("DELETE FROM history");
+        statement.close();
     }
 
     // Writing to Database //
+    /** Writes a purchase to the purchase table in the database */
     private void writePurchase(Purchase purchase) throws SQLException {
-        Statement.executeUpdate("INSERT INTO purchase VALUES ('" +
+        Statement statement = Connection.createStatement();
+        statement.executeUpdate("INSERT INTO purchase VALUES ('" +
                 purchase.getDateAsString() + "', " +
                 purchase.getDate().getMonthValue() + ", " +
                 purchase.getNumberHamburger()+ ", " +
                 purchase.getNumberSandwich()+ ", " +
                 purchase.getNumberStrip() + ");");
+        statement.close();
     }
-
-    //TODO - Refactor to work with new purchase history class
     /** Writes a purchase history to the database */
     public void writePurchaseHistory (PurchaseHistory purchaseHistory) throws SQLException {
-        Statement.executeUpdate("INSERT INTO history VALUES (" +
+        Statement statement = Connection.createStatement();
+        statement.executeUpdate("INSERT INTO history VALUES (" +
                 purchaseHistory.getMonth() + ", " +
                 purchaseHistory.getSupplyCost() +");");
         for (Purchase purchase : purchaseHistory.getAllPurchases()) {
             writePurchase(purchase);
         }
+        statement.close();
     }
 
-    //TODO - Refactor and cleanup, also comment everything; maybe rename everything to make more sense
 
     // Fetching Purchase History //
-    /*public PurchaseHistory fetchPurchaseHistory(int month) throws PurchaseDoesNotExistException{
-        try {
-            if (historyExists(month)) {
-                ResultSet historyInformation = Statement.executeQuery("SELECT * FROM " +
-                        "history WHERE month = " + month);
-                historyInformation.next();
-                int burgerSupply = historyInformation.getInt(2);
-                int chickenSupply = historyInformation.getInt(3);
-                ArrayList<Purchase> purchaseList = new ArrayList<>();
-                historyInformation.close();
-                ResultSet purchaseInformation = Statement.executeQuery("SELECT * FROM " +
-                        "purchase WHERE month = " + month);
-                while (purchaseInformation.next()) {
-                    String purchaseDate = purchaseInformation.getString(1);
-                    int burger = purchaseInformation.getInt(3);
-                    int strip = purchaseInformation.getInt(4);
-                    int sandwich = purchaseInformation.getInt(5);
-                    ArrayList<Product> productList = new ArrayList<>();
-                    while (burger != 0) {
-                        productList.add(Product.HAMBURGER);
-                        burger--;
-                    }
-                    while (strip != 0) {
-                        productList.add(Product.CHICKEN_STRIPS);
-                        strip--;
-                    }
-                    while (sandwich != 0) {
-                        productList.add(Product.CHICKEN_SANDWICH);
-                        sandwich--;
-                    }
-                    for (Product product : productList) {System.out.println(product.toString());}
-                    purchaseList.add(new Purchase(purchaseDate, productList));
-                }
-                for (Purchase p : purchaseList) {System.out.println(p.getProfitMade());}
-                purchaseInformation.close();
-                return new PurchaseHistory(burgerSupply, chickenSupply, month, purchaseList);
-            } else {
-                System.err.println("ERR: Tried to fetch purchase history data that did not exist!");
-                throw new PurchaseDoesNotExistException("Purchase History doesn't exist!");
-            }
-        } catch (SQLException sql_exception) {
-            System.err.println("ERR: SQL Exception while fetching purchase history!");
-            System.err.println(sql_exception.getMessage());
+    /**Fetch the Purchase History for a given month from the database*/
+    public PurchaseHistory fetchPurchaseHistory(int month) throws PurchaseDoesNotExistException{
+        if (historyDataExists(month)) {
+            ArrayList<Purchase> purchaseList = fetchPurchaseData(month);
+            double supplyCost = fetchSupplyCost(month);
+            return new PurchaseHistory(supplyCost, month, purchaseList);
+        } else {
+            System.err.println("ERR: Tried to fetch purchase history data that did not exist!");
+            throw new PurchaseDoesNotExistException("Purchase History doesn't exist!");
         }
-        return null;
     }
-    private boolean historyExists(int month) throws SQLException {
-        ResultSet resultSet = Statement.executeQuery("SELECT EXISTS( SELECT * FROM " +
-                "history WHERE month = "+ month +");");
-        resultSet.next(); //Have to do this prior to reading the result set, or the program errors
-        boolean exists = (resultSet.getInt(1) == 1);
-        resultSet.close();
-        return exists;
-    }*/
+    /** Checks if the given Purchase History Data exists in the database */
+    private boolean historyDataExists(int month) {
+        try {
+            Statement statement = Connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT EXISTS( SELECT * FROM " +
+                    "history WHERE month = "+ month +");");
+            resultSet.next(); //Have to do this prior to reading the result set, or the program errors
+            boolean exists = (resultSet.getInt(1) == 1);
+            resultSet.close();
+            statement.close();
+            return exists;
+        } catch (SQLException sqlException) {
+            System.err.println("ERR: Failure to check existence of purchase history! Returning false!");
+            System.err.println(sqlException.getMessage());
+            return false;
+        }
+    }
+    /** Fetches the purchase history's supply cost from the history table*/
+    private double fetchSupplyCost (int month) {
+        try {
+            double supplyCost = 0.0;
+            Statement statement = Connection.createStatement();
+            ResultSet historyInformation = statement.executeQuery("SELECT * FROM " +
+                    "history WHERE month = " + month);
+            historyInformation.next();
+            supplyCost = historyInformation.getDouble(2);
+            historyInformation.close();
+            statement.close();
+            return  supplyCost;
+        } catch (SQLException sqlException) {
+            System.err.println("ERR: Failed to fetch supply cost! Returning 0.0!");
+            System.err.println(sqlException.getMessage());
+            return 0.0;
+        }
+    }
+    /** Fetches a list of purchases from a given month */
+    private ArrayList<Purchase> fetchPurchaseData(int month) {
+        ArrayList<Purchase> purchaseList = new ArrayList<>();
+        try {
+            Statement statement = Connection.createStatement();
+            ResultSet purchaseInformation = statement.executeQuery("SELECT * FROM " +
+                    "purchase WHERE month = " + month);
+            while (purchaseInformation.next()) {
+                String purchaseDate = purchaseInformation.getString(1);
+                int burger = purchaseInformation.getInt(3);
+                int strip = purchaseInformation.getInt(4);
+                int sandwich = purchaseInformation.getInt(5);
+                ArrayList<Product> productList = new ArrayList<>();
+                while (burger != 0) {
+                    productList.add(Product.HAMBURGER);
+                    burger--;
+                }
+                while (strip != 0) {
+                    productList.add(Product.CHICKEN_STRIPS);
+                    strip--;
+                }
+                while (sandwich != 0) {
+                    productList.add(Product.CHICKEN_SANDWICH);
+                    sandwich--;
+                }
+                for (Product product : productList) {System.out.println(product.toString());}
+                purchaseList.add(new Purchase(purchaseDate, productList));
+            }
+            purchaseInformation.close();
+            statement.close();
+        } catch (SQLException sqlException) {
+            System.err.println("ERR: Failed to fetch purchase data!");
+            System.err.println(sqlException.getMessage());
+        }
+        return purchaseList;
+    }
 }
